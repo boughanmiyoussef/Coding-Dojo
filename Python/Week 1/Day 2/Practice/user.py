1a292d7b6b659819e88177cff094f725eb480982
boughanmiyoussef <yussefboughanmy@gmail.com>
2023-11-15 10:16:54 +0100

    commit

---
+++ Python/Week 1/Day 2/Practice/user.py
@@ -0,0 +1,54 @@
+class User:
+    
+    def __init__(self, first_name, last_name, email, age, is_rewards_member=False, gold_card_points=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print(f"Name: {self.first_name} {self.last_name}")
+        print(f"Email: {self.email}")
+        print(f"Age: {self.age}")
+        print(f"Member: {self.is_rewards_member}")
+        print(f"Points: {self.gold_card_points}")
+
+    def enroll(self):
+        self.is_rewards_member = True
+        self.gold_card_points = 200 
+        if (self.is_rewards_member == True):
+            print("User is already a member")
+            return False
+        else:
+            return True
+
+
+    def spend_points(self,amount):
+        self.gold_card_points=100
+        if(self.gold_card_points < amount ):
+            return False
+
+
+
+user1=User("Ahmed" , "Aziz" , "abc@gmail.com" , "300")
+print(user1)
+user1.display_info()
+
+
+user1.enroll()
+
+
+user2=User("Jack" , "Mack" , "xyz@gmail.com" ,"90")
+print(user2)
+user2.display_info()
+
+
+
+
+
+
+
+
+
